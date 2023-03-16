a01aca4ccfacc270780471dfed8b700a2571960e
Eliel Dantas <80924505+mrdantas@users.noreply.github.com>
2023-03-16 10:39:11 -0300

    Update pagespeed_validator.py
    Took the API key

--- validator/pagespeed_validator.py
+++ validator/pagespeed_validator.py
@@ -3,7 +3,7 @@ import json
 
 class PagespeedValidator():
     def get_pagespeed(self, url):
-        apiKey = 'AIzaSyC7CeAKw5Z-JZzkP9tY3kP6YsBQgPWk-ZA'
+        apiKey = 'ourApiKey'
         pagespeedUrl = url.replace(':', '%3A')
         pagespeedUrl = url.replace('/', '%2F')
         mobileUrl = f'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={pagespeedUrl}&category=performance&locale=pt_BR&strategy=mobile&key={apiKey}'
